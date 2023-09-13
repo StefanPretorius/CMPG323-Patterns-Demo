26667fd61151853d234fc16a1c6e4ff4169945c3
Stefan Pretorius <stefanpretorius0313@gmail.com>
2023-09-13 21:31:08 +0200

    Added tier 1 pattern

---
+++ HomeWork-Patterns/HomeWork-Patterns/Controllers/ServicesController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HomeWork_Patterns.Data;
+using HomeWork_Patterns.Models;
+using HomeWork_Patterns.Repository;
+
+namespace HomeWork_Patterns.Controllers
+{
+    public class ServicesController : Controller
+    {
+        private readonly CMPG_323_Patterns_HomeworkContext _context;
+
+        public ServicesController(CMPG_323_Patterns_HomeworkContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Services
+        public async Task<IActionResult> Index()
+        {
+            ServiceRepository serviceRepository = new ServiceRepository();
+
+            var results = serviceRepository.GetAll();
+
+            return View(results);
+        }
+
+        // GET: Services/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null || _context.Services == null)
+            {
+                return NotFound();
+            }
+
+            var service = await _context.Services
+                .FirstOrDefaultAsync(m => m.ServiceId == id);
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            return View(service);
+        }
+
+        // GET: Services/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Services/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ServiceId,ServiceName,ServiceDescription,CreatedDate")] Service service)
+        {
+            if (ModelState.IsValid)
+            {
+                service.ServiceId = Guid.NewGuid();
+                _context.Add(service);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(service);
+        }
+
+        // GET: Services/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null || _context.Services == null)
+            {
+                return NotFound();
+            }
+
+            var service = await _context.Services.FindAsync(id);
+            if (service == null)
+            {
+                return NotFound();
+            }
+            return View(service);
+        }
+
+        // POST: Services/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("ServiceId,ServiceName,ServiceDescription,CreatedDate")] Service service)
+        {
+            if (id != service.ServiceId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(service);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ServiceExists(service.ServiceId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(service);
+        }
+
+        // GET: Services/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null || _context.Services == null)
+            {
+                return NotFound();
+            }
+
+            var service = await _context.Services
+                .FirstOrDefaultAsync(m => m.ServiceId == id);
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            return View(service);
+        }
+
+        // POST: Services/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            if (_context.Services == null)
+            {
+                return Problem("Entity set 'CMPG_323_Patterns_HomeworkContext.Services'  is null.");
+            }
+            var service = await _context.Services.FindAsync(id);
+            if (service != null)
+            {
+                _context.Services.Remove(service);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ServiceExists(Guid id)
+        {
+          return (_context.Services?.Any(e => e.ServiceId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ HomeWork-Patterns/HomeWork-Patterns/Repository/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using HomeWork_Patterns.Data;
+using HomeWork_Patterns.Models;
+
+namespace HomeWork_Patterns.Repository
+{
+    public class ProductRepository
+    {
+        protected readonly CMPG_323_Patterns_HomeworkContext _context = new CMPG_323_Patterns_HomeworkContext();
+
+        // GET ALL: Products
+        public IEnumerable<Product> GetAll()
+        {
+            return _context.Products.ToList();
+        }
+
+        public IEnumerable<Product> Details(Guid? id)
+        {
+            return _context.Products.ToList();
+        }
+
+    }
+}
